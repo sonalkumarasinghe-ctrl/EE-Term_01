e5566411977db042cbe6365bdaa3d515d53ad3da
sonalkumarasinghe-ctrl <sonalkumarasinghe544@gmail.com>
2025-09-20 08:33:07 +0530

    Update python.py

--- python.py
+++ python.py
@@ -3,5 +3,12 @@ def christmas_tree(x):
     for r in range(0,x):
         print(" " * (x - r - 1) + "*" * (2 * r + 1))
       
-
-print('Hello World')      
+ ## x0 = "apple", y0 = "adcsjncjsppaxjjnaxle" --> True if all characters of x0 are in y0 in order, y0 = "bsdpple" --> False, y0 = "paple" --> False
+def detector(x0,y0):
+    extractor=''
+    for letter in y0:
+        if letter in x0:extractor+=letter
+    if extractor==x0:
+        print('True')
+    else:
+        print('False')        
