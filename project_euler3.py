a9cc953910d88a9c7e39d8d5406785d14c3a24c6
sonalkumarasinghe-ctrl <sonalkumarasinghe544@gmail.com>
2025-09-25 11:21:35 +0530

    Update project_euler3.py

--- project_euler3.py
+++ project_euler3.py
@@ -2,11 +2,15 @@ from math import*
 def is_prime(num):
     a=True
     for r in range(2,int(sqrt(num)+1)):
-        if num%r==0:
+        if num%r==0 or num!=2:
             return False
     else:
         return a    
-b=is_prime(11) 
-print(b)       
+num=int(input("Enter the Val-:"))
+Result=is_prime(num)
+for r in range(num,2,-1):
+    
+    
+
             
         
