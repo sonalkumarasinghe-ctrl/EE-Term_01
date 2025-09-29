270f85c5bf60de4475f0ddddafd1c849dfee8d95
sonalkumarasinghe-ctrl <sonalkumarasinghe544@gmail.com>
2025-09-30 00:00:03 +0530

    updated

--- project_euler9.py
+++ project_euler9.py
@@ -1,12 +1,17 @@
 from math import*
 val=None
-for r in range(1,1000):
-    if val==None:
-        for s in range(r,1000):
-            py=sqrt(r**2+s**2)
-            if int(py)==py and py<1000:
-                val=r*s*py
+con=False
+a,b,c=1,1,1
+for a in range(1,1000):
+    if con==False:
+        for b in range(a,1000-a):
+            # print(sqrt(a**2+b**2))==sqrt(a**2+b**2),print(a+b+a**2+b**2==1000)
+            # if int(sqrt(a**2+b**2))==sqrt(a**2+b**2) and a+b+a**2+b**2==1000:
+            #     print(a*b*(a**2+b**2))
+            #     con=True
+            #     break
+            c=1000-a-b
+            if c**2==a**2+b**2:
+                print(a*b*c)
+                con=True
                 break
-    else:
-        break        
-print(val)        
